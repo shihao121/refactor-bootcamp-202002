920362604f2fd59ddc2bf275920b6be76db65731
shihao <hao.shi@thoughtworks.com>
2020-02-16 00:06:14 +0800

    [shihao] refactor: extract method.

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/CalculateUtil.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/CalculateUtil.java
@@ -2,9 +2,13 @@ package cc.xpbootcamp.warmup.fibonacci;
 
 public class CalculateUtil {
     public static int calculate(int position) {
+        validatePosition(position);
+        return 1;
+    }
+
+    private static void validatePosition(int position) {
         if (position > 50 || position <= 0) {
             throw new IllegalArgumentException("out of border");
         }
-        return 1;
     }
 }
