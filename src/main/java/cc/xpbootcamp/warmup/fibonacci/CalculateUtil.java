f94a1708acdf230d0d81d2fcbe7b51dbce816db2
shihao <hao.shi@thoughtworks.com>
2020-02-16 00:04:58 +0800

    [shihao] feat: can only get top fifty fibonacci number.

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/CalculateUtil.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/CalculateUtil.java
@@ -2,6 +2,9 @@ package cc.xpbootcamp.warmup.fibonacci;
 
 public class CalculateUtil {
     public static int calculate(int position) {
+        if (position > 50 || position <= 0) {
+            throw new IllegalArgumentException("out of border");
+        }
         return 1;
     }
 }
--- src/test/java/cc/xpbootcamp/warmup/fibonacci/CalculateUtilTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/CalculateUtilTest.java
@@ -1,6 +1,8 @@
 package cc.xpbootcamp.warmup.fibonacci;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -8,5 +10,13 @@ class CalculateUtilTest {
     @Test
     void should_return_1_when_calculate_given_position_is_1() {
         assertEquals(1, CalculateUtil.calculate(1));
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {0, 51, 52})
+    void should_throw_exception_when_position_is_out_of_border(int position) {
+        assertThrows(IllegalArgumentException.class, () -> {
+            CalculateUtil.calculate(position);
+        });
     }
 }
