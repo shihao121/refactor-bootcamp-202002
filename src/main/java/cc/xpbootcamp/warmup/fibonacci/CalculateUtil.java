0719d7124db89c68ea576cd40a78fe176a17c3b8
shihao <hao.shi@thoughtworks.com>
2020-02-15 23:54:14 +0800

    [shihao] refactor: rename.

--- src/main/java/cc/xpbootcamp/warmup/fibonacci/CalculateUtil.java
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/CalculateUtil.java
@@ -1,7 +1,7 @@
 package cc.xpbootcamp.warmup.fibonacci;
 
 public class CalculateUtil {
-    public static int Calculate(int position) {
+    public static int calculate(int position) {
         return 1;
     }
 }
--- src/test/java/cc/xpbootcamp/warmup/fibonacci/CalculateUtilTest.java
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/CalculateUtilTest.java
@@ -7,6 +7,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class CalculateUtilTest {
     @Test
     void should_return_1_when_calculate_given_position_is_1() {
-        assertEquals(1, CalculateUtil.Calculate(1));
+        assertEquals(1, CalculateUtil.calculate(1));
     }
 }
