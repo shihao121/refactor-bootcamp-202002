d8405d6e60e9485316fe9d8881ed9cb88fec5d8b
shihao <hao.shi@thoughtworks.com>
2020-02-15 23:49:20 +0800

    [shihao] feat: get the fibonacci value of the first position.

---
+++ src/main/java/cc/xpbootcamp/warmup/fibonacci/CalculateUtil.java
@@ -0,0 +1,7 @@
+package cc.xpbootcamp.warmup.fibonacci;
+
+public class CalculateUtil {
+    public static int Calculate(int position) {
+        return 1;
+    }
+}
---
+++ src/test/java/cc/xpbootcamp/warmup/fibonacci/CalculateUtilTest.java
@@ -0,0 +1,12 @@
+package cc.xpbootcamp.warmup.fibonacci;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CalculateUtilTest {
+    @Test
+    void should_return_1_when_calculate_given_position_is_1() {
+        assertEquals(1, CalculateUtil.Calculate(1));
+    }
+}
